71c02b7b47f3015e5613ce70301dcfc80100ca44
miuru <miurushalinda@gmail.com>
2020-02-29 13:16:19 +0530

    added splash screen

---
+++ lib/splashScreen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+import 'main.dart';
+
+void main() {
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => new _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+        seconds: 5,
+        navigateAfterSeconds: new BatteryLevelPage(),
+        title: new Text(
+          'Welcome to My Battery App',
+          style: new TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurpleAccent,
+              fontSize: 25.0,
+              ),
+        ),
+        image: Image.asset("images/batterySplash.png"),
+        backgroundColor: Colors.black,
+        styleTextUnderTheLoader: new TextStyle(
+            fontSize: 45.0,
+            color: Colors.deepPurpleAccent,
+            fontWeight: FontWeight.bold,
+           ),
+        photoSize: 120.0,
+        onClick: () => print("Flutter Battery"),
+        loaderColor: Colors.deepPurpleAccent);
+  }
+}
